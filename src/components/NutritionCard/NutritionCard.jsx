63603f42ce1bff19b8260ef2a9699c3169839347
aalexandree-g <aalexandree.g@gmail.com>
2025-09-27 22:35:48 +0200

    Layout + nutrition cards done

---
+++ src/components/NutritionCard/NutritionCard.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+export default function NutritionCard({ icon, value, unit, label }) {
+  return (
+    <div className="nutrition-card">
+      <div
+        className={`nutrition-card__icon nutrition-card__icon--${label.toLowerCase()}`}
+      >
+        <img src={icon} alt={label} />
+      </div>
+      <div className="nutrition-card__info">
+        <p className="nutrition-card__value">
+          {value.toLocaleString('en-US')}
+          {unit}
+        </p>
+        <p className="nutrition-card__label">{label}</p>
+      </div>
+    </div>
+  )
+}
+
+NutritionCard.propTypes = {
+  icon: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  unit: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+}
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getUser } from '../../api/client'
+import NutritionCard from '../../components/NutritionCard/NutritionCard'
+import caloriesIcon from '../../assets/calories-icon.png'
+import proteinIcon from '../../assets/protein-icon.png'
+import carbsIcon from '../../assets/carbs-icon.png'
+import fatIcon from '../../assets/fat-icon.png'
 
 export default function Dashboard() {
   const { id } = useParams()
@@ -14,13 +19,61 @@ export default function Dashboard() {
 
   return (
     <div className="dashboard">
-      <div className="dashboard__welcome">
-        <h1>
-          Bonjour <span>{user.userInfos.firstName}</span>
+      <div className="dashboard__header">
+        <h1 className="dashboard__title">
+          Bonjour{' '}
+          <span className="dashboard__username">
+            {user.userInfos.firstName}
+          </span>
         </h1>
-        <p>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
+        <p className="dashboard__subtitle">
+          Félicitations ! Vous avez explosé vos objectifs hier 👏
+        </p>
       </div>
-      <div className="dashboard__graphs">bonjour</div>
+      <div className="dashboard__content">
+        <div className="dashboard__performances">
+          <div className="dashboard__chart"></div>
+          <div className="dashboard__charts">
+            <div className="dashboard__card"></div>
+            <div className="dashboard__card"></div>
+            <div className="dashboard__card"></div>
+          </div>
+        </div>
+        <div className="dashboard__nutrition">
+          <div className="dashboard__card">
+            <NutritionCard
+              icon={caloriesIcon}
+              value={user.keyData.calorieCount}
+              unit="kCal"
+              label="Calories"
+            />
+          </div>
+          <div className="dashboard__card">
+            <NutritionCard
+              icon={proteinIcon}
+              value={user.keyData.proteinCount}
+              unit="g"
+              label="Protéines"
+            />
+          </div>
+          <div className="dashboard__card">
+            <NutritionCard
+              icon={carbsIcon}
+              value={user.keyData.carbohydrateCount}
+              unit="g"
+              label="Glucides"
+            />
+          </div>
+          <div className="dashboard__card">
+            <NutritionCard
+              icon={fatIcon}
+              value={user.keyData.lipidCount}
+              unit="g"
+              label="Lipides"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
